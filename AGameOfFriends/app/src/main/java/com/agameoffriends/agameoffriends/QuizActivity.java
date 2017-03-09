aaf2bd0a2ccc6e56f3c316dec81193c5c459302f
jsannyjr <jsanny1@lion.lmu.edu>
2017-03-09 14:16:46 -0800

    Gave buttons some functionality.
    Will work on listeners to get the buttons to move to the next quesiton.

--- AGameOfFriends/app/src/main/java/com/agameoffriends/agameoffriends/QuizActivity.java
+++ AGameOfFriends/app/src/main/java/com/agameoffriends/agameoffriends/QuizActivity.java
@@ -4,13 +4,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private QuestionLibrary questionLibrary = new QuestionLibrary();
+
+    private TextView mScoreView;
+    private TextView mQuestionView;
+    private Button nButtonChoice0;
+    private Button nButtonChoice1;
+    private Button nButtonChoice2;
+    private Button nButtonChoice3;
+
+    private String nAnswer;
+    private int mScore = 0;
+    private int mQuestioNumber = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mScoreView = (TextView)findViewById(R.id.score);
+        mQuestionView =(TextView)findViewById(R.id.question);
+        nButtonChoice0 = (Button)findViewById(R.id.choice1);
+        nButtonChoice1 = (Button)findViewById(R.id.choice2);
+        nButtonChoice2 = (Button)findViewById(R.id.choice3);
+        nButtonChoice3 = (Button)findViewById(R.id.choice4);
     }
 
     @Override
