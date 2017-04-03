b1f9803fe3424e350ccd1b4c8dd019231999b8d3
jsannyjr <jsanny1@lion.lmu.edu>
2017-04-03 12:34:27 -0700

    Started on Main Menu screen
    Need to add funtionality to multiple buttons.

---
+++ AGameOfFriends/app/src/main/java/com/agameoffriends/agameoffriends/MainActivity.java
@@ -0,0 +1,9 @@
+package com.agameoffriends.agameoffriends;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Sanny on 4/3/2017.
+ */
+public class MainActivity extends AppCompatActivity{
+}
