6da54ae52ea743af368d2682b7dc84475f3ac177
jsannyjr <jsanny1@lion.lmu.edu>
2017-04-05 16:51:56 -0700

    Got Main Menu started
    The New Game button starts. Need to finish exit button.

--- AGameOfFriends/app/src/main/java/com/agameoffriends/agameoffriends/MainActivity.java
+++ AGameOfFriends/app/src/main/java/com/agameoffriends/agameoffriends/MainActivity.java
@@ -1,9 +1,31 @@
 package com.agameoffriends.agameoffriends;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by Sanny on 4/3/2017.
  */
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
+    Button mStartButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mStartButton = (Button)findViewById(R.id.startButton);
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(MainActivity.this, QuizActivity.class));
+            }
+        });
+    }
 }
