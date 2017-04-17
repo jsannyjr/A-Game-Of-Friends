8610b5d1d6ed687ae9ebb967142205dc59553419
jsannyjr <jsanny1@lion.lmu.edu>
2017-04-16 21:53:05 -0700

    Starting new score menu.

---
+++ AGameOfFriends/app/src/main/java/com/agameoffriends/agameoffriends/ScoreActivity.java
@@ -0,0 +1,30 @@
+package com.agameoffriends.agameoffriends;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ScoreActivity extends AppCompatActivity{
+    Button mFinishButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+        mFinishButton = (Button)findViewById(R.id.finishButton);
+        mFinishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ScoreActivity.this, MainActivity.class));
+            }
+        });
+
+    }
+}
